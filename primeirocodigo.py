a0ccd5577a343f9c1a0c90222ee1eaafd0d83f30
LeozinhoXtx <leocoelho1908@hotmail.com>
2025-02-18 20:35:48 -0300

    V2

--- primeirocodigo.py
+++ primeirocodigo.py
@@ -1,2 +1,20 @@
 caixavazia=input('Digite o seu nome')
 print (caixavazia)
+
+
+
+#Agora vou escrever um codigo para identificar o tipo de dado de uma variavel
+caixa='Ola,Mundo'
+print(type(caixa))
+
+#Agora vou escrever um codigo para identificar o tipo de dado de uma variavel que recebe numeros
+variavel_numero=3.2
+print(type(variavel_numero))
+
+
+
+#Agora vou escrever um codigo para identificar o tipo de dado de uma variavel booleana
+
+
+variavel_booleana= 10>3
+print(type(variavel_booleana))
