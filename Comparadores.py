bea6badc8ce2745630d5fbe3d871d6c161f7604e
LeozinhoXtx <leocoelho1908@hotmail.com>
2025-02-22 10:44:45 -0300

    Add files via upload

---
+++ Comparadores.py
@@ -0,0 +1,13 @@
+valor1 = int(input('Digite o valor1: '))
+valor2 = int(input('Digite o valor2: '))
+
+
+maior_que = valor1 > valor2 #VAI ARMAZENAR NA CAIXA MAIOR_QUE,SE O RESULTADO DE COMPARAÇAO E TRUE OU FALSE
+menor_que = valor1 < valor2 
+igual_a = valor1 == valor2
+diferente_De = valor1 != valor2
+
+print(maior_que)
+print(menor_que)
+print(igual_a)
+print(diferente_De)
