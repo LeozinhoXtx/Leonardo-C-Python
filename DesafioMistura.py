98ea908154f6a8e0f91c95b9322fc34ffddcb06e
LeozinhoXtx <leocoelho1908@hotmail.com>
2025-03-26 00:33:37 -0300

    Add files via upload

---
+++ DesafioMistura.py
@@ -0,0 +1,35 @@
+#Peça para o usuário informar quantas misturas ele quer verificar.
+Cliente= input('Boa tarde,td bem? Quero verificar alguns teste de mistura.Com as seguintes porcetagem!')
+porcentagem_milho= print('Milho :70%')
+porcentagem_soja= print('Soja :25%')
+porcentagem_nucleo= print('Nucleo Vitaminico :5%')
+
+
+Teste_Raçoes= input ('Boa tarde ,tudo bem sim? Quantos teste de misturas voce quer verificar? ')
+
+#Para cada mistura, o programa deve pedir os valores de porcentagem dos três ingredientes.
+
+
+Mistura_1= print('Mistura 1:')
+input(' Porcentagem de milho:')
+input('Porcentagem de soja:')
+input('Porcentagem de nucleo vitaminico:')
+
+
+if Mistura_1!=Cliente:
+ print('Mistura;REPROVADA')
+
+
+else : Mistura_1== Cliente
+print('Mistura:APROVADA')
+
+Mistura_2= print('Mistura 2:')
+input('Porcentagem de milho:')
+input('Porcentagem de soja:')
+input('Porcentagem de nucleo vitaminico:')
+
+if Mistura_2!=Cliente:
+ print('Mistura;REPROVADA')
+
+else:Mistura_2 == Cliente
+print('Mistura:APROVADA')
