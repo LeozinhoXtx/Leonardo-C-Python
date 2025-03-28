b07de36abd8deb35188cee30ad680ceff0b6e6e0
LeozinhoXtx <leocoelho1908@hotmail.com>
2025-03-27 21:36:57 -0300

    Add files via upload

--- DesafioMistura.py
+++ DesafioMistura.py
@@ -1,35 +1,72 @@
 #Peça para o usuário informar quantas misturas ele quer verificar.
-Cliente= input('Boa tarde,td bem? Quero verificar alguns teste de mistura.Com as seguintes porcetagem!')
-porcentagem_milho= print('Milho :70%')
-porcentagem_soja= print('Soja :25%')
-porcentagem_nucleo= print('Nucleo Vitaminico :5%')
+Cliente= input('Boa tarde,td bem? Quero verificar alguns teste de mistura.')
+
+Teste_Raçoes= input ('Boa tarde ,tudo bem sim? Quantos teste de misturas voce quer verificar?:')
 
 
-Teste_Raçoes= input ('Boa tarde ,tudo bem sim? Quantos teste de misturas voce quer verificar? ')
+porcentagem_milho1=input('Qual porcentagem do milho:')
+porcentagem_soja1=input('Qual porcentagem do soja:')
+porcentagem_nucleo1=input('Qual porcentagem do nucleo vitaminico:')
 
 #Para cada mistura, o programa deve pedir os valores de porcentagem dos três ingredientes.
 
 
 Mistura_1= print('Mistura 1:')
-input(' Porcentagem de milho:')
-input('Porcentagem de soja:')
-input('Porcentagem de nucleo vitaminico:')
+milho=input('Porcentagem de milho:')
+soja=input('Porcentagem de soja:')
+nucleo=input('Porcentagem de nucleo:')
+
+if porcentagem_milho1==milho and porcentagem_soja1==soja and porcentagem_nucleo1==nucleo:
+ print('Mistura;Aprovada')
+
+else:
+ print('Mistura REPROVADA')
 
 
-if Mistura_1!=Cliente:
- print('Mistura;REPROVADA')
+Mistura_2=print('Mistura 2:')
+milho=input('Porcentagem de milho:')
+soja=input('Porcentagem de soja:')
+nucleo=input('Porcentagem de nucleo:')
+
+if porcentagem_milho1==milho and porcentagem_soja1==soja and porcentagem_nucleo1==nucleo:
+ print('Mistura;Aprovada')
+
+else:
+ print('Mistura REPROVADA')
 
 
-else : Mistura_1== Cliente
-print('Mistura:APROVADA')
 
-Mistura_2= print('Mistura 2:')
-input('Porcentagem de milho:')
-input('Porcentagem de soja:')
-input('Porcentagem de nucleo vitaminico:')
 
-if Mistura_2!=Cliente:
- print('Mistura;REPROVADA')
 
-else:Mistura_2 == Cliente
-print('Mistura:APROVADA')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Milho certo:ok
+#Soja certo:ok
+#Nucleo certo:ok
+
+
+#Milho errado:ok
+#Soja certo:ok
+#Nucleo certo:ok
+
+#Milho certo:ok
+#Soja errado:ok
+#Nucleo certo: ok
+ 
+
+ #Milho certo:ok
+#Soja certo:ok
+#Nucleo errado:ok
