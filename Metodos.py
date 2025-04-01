17b2235f2c426ab42883a2eb85ccc0d8bef8a9e4
LeozinhoXtx <leocoelho1908@hotmail.com>
2025-04-01 20:31:34 -0300

    Add files via upload

---
+++ Metodos.py
@@ -0,0 +1,9 @@
+#Quero importar biblioteca RANDOM e utiliziar seus metodos ja prontos
+import random
+
+#Metodo randint:ele seleciona numero aleatorio dentro de um intervalo
+print(random.randint(1,10))
+
+#Metodo Choice:Ele escolhe um elemento dentro de uma lista opçoes
+print(random.choice(['Pera','maça','uva','morango']))
+
