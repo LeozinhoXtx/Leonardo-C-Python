97b07542030985e7ccc14f4615b2775b75865eff
LeozinhoXtx <leocoelho1908@hotmail.com>
2025-04-08 20:32:24 -0300

    Add files via upload

---
+++ ManipStrings.py
@@ -0,0 +1,8 @@
+#Validando posiçoes dentro de um texto
+
+#Python inicia a contagem sempre em "ZERO"
+ 
+Nome='Leonardo'
+#imprimir posiçao 3 da strings(texto) que esta armazenado em nom
+print (Nome [3])  #conta da esquerda pra direita
+print(Nome [-5]) #conta da direita pra esquerda
