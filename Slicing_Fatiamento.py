3faa0abd4d64fdfcd1a9dc70aefa1a439a31eda4
LeozinhoXtx <leocoelho1908@hotmail.com>
2025-05-27 20:36:12 -0300

    Add files via upload

---
+++ Slicing_Fatiamento.py
@@ -0,0 +1,9 @@
+#Slicing(FATIAMENTO)=Tecnica de seleçao de elementos DENTRO DE (STRING,TUPLA,LISTAS)
+#INICIO:FIM:PASSO
+tupla_nomes=('Alvaro','Leticia','Bruno','Henrique','Carlos')
+
+print(tupla_nomes[1:4])
+print(tupla_nomes[:3])
+print(tupla_nomes[2:])
+print(tupla_nomes[::2])
+print(tupla_nomes[::-1])
